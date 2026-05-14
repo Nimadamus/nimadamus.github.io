747e62e6e5ad91f4af1352617dd59cd5f3ca9dfe
Nimadamus <nj2121@gmail.com>
2026-05-14 14:12:33 -0700

    Sync homepage pick statuses and article previews

--- homepage-picks-data.js
+++ homepage-picks-data.js
@@ -48,6 +48,7 @@ var HOMEPAGE_PICKS = [
         sport: "NHL",
         title: "Vegas Golden Knights Moneyline -150 vs Ducks Game 5",
         date: "May 12, 2026",
+        result: "W",
         url: "vegas-golden-knights-moneyline-minus-150-ducks-game-5-nhl-pick.html",
         image: "https://media.d3.nhle.com/image/private/t_ratio16_9-size50/v1778591751/prd/bjchqodtuj95prh4wz2d.png"
     },
@@ -62,6 +63,7 @@ var HOMEPAGE_PICKS = [
         sport: "NBA",
         title: "Lakers +11 vs Thunder Game 4, Elimination Spot Creates Double-Digit Spread Value",
         date: "May 11, 2026",
+        result: "W",
         url: "lakers-plus-11-thunder-game-4-elimination-spot-nba-pick.html",
         image: "images/sacramento-kings-oklahoma-city-thunder-nba-over-betting-pick-total-november-19-2025.webp"
     },
